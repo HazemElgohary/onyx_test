c3a4990587a58ef493ea3f65bfe77a07aab692a3
Hazem Elgohary <hazemelgohary@Hazems-MacBook-Air.local>
2024-02-25 14:58:13 +0200

    create login and splash screens routes

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,13 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const LOGIN_SCREEN = _Paths.LOGIN_SCREEN;
+  static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const LOGIN_SCREEN = '/login-screen';
+  static const SPLASH_SCREEN = '/splash-screen';
 }
